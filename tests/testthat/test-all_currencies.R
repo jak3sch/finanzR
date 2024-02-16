79d7f17d726918d8985aaad2b484737eebd7a134
Jakob Eschler <mail@jakob-eschler.de>
2024-02-16 10:25:40 +0100

    fixed all_currencies() test

--- tests/testthat/test-all_currencies.R
+++ tests/testthat/test-all_currencies.R
@@ -1,4 +1,4 @@
-test_that("all_currencies returns tibble with specific col names", {
+test_that("all_currencies() returns tibble with specific col names", {
   currencies <- finanzR::all_currencies()
 
   testthat::expect_type(currencies, "list")
