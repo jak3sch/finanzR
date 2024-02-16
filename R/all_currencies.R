14c92206f6cd575f3f00d04e5ad8c8f118e2cc0c
Jakob Eschler <mail@jakob-eschler.de>
2024-02-16 09:14:15 +0100

    added tests for all_currencies()

--- R/all_currencies.R
+++ R/all_currencies.R
@@ -2,21 +2,24 @@
 #'
 #' @description Load all supported currencies. It uses the [kraken](https://support.kraken.com/hc/en-us/articles/201893658-Currency-pairs-available-for-trading-on-Kraken) cash-to-crypto pairs.
 #'
-#' @return A character string with all currency IDs
+#' @return Returns a tibble with the following columns:
+#' * `symbol` (character): currency symbol (e.g. USD, EUR...);
+#' * `name` (character): currency name;
+#' *`currency_id` (character): currency id to sync with kraken assets (e.g. ZEUR)
 #'
 #' @importFrom magrittr %>%
-#' @importFrom rlang .data
 #'
 #' @export
 #'
 #' @examples
 #' r <- all_currencies()
 #' r
+
 all_currencies <- function() {
   kraken_cash_to_crypto_coin_currencies <-c("USD", "EUR", "CAD", "JPY", "GBP", "CHF", "AUD")
   currency_names <- c("US Dollar", "Euro", "Canadian Dollar", "Yen", "Pound Sterling", "Swiss franc", "Australian Dollar")
 
-  currencies <- tibble(kraken_cash_to_crypto_coin_currencies, currency_names, .name_repair = ~ c("symbol", "name")) %>%
+  currencies <- tibble::tibble(kraken_cash_to_crypto_coin_currencies, currency_names, .name_repair = ~ c("symbol", "name")) %>%
     dplyr::mutate(currency_id = paste0("Z", symbol))
 
   return(currencies)
---
+++ tests/testthat/test-all_currencies.R
@@ -0,0 +1,7 @@
+test_that("all_currencies returns tibble with specific col names", {
+  currencies <- finanzR::all_currencies()
+
+  testthat::expect_type(currencies, "list")
+  testthat::expect_named(currencies, c("symbol", "name", "currency_id"))
+})
+
